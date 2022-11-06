ece7509ec74af177e5985a727f4526acf429c778
isachomonik <dev.isachomonik@gmail.com>
2022-11-06 15:39:06 -0300

    working just fine

---
+++ script.js
@@ -0,0 +1,88 @@
+// variables
+
+let workTime = 25;
+let breakTime = 5;
+
+let seconds = '00';
+
+// display
+
+
+
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    document.getElementById('start').innerHTML = 'Start'
+
+    document.getElementById('reset').style.display = 'none';
+
+}
+
+// star timer
+
+
+
+function remTime(){
+    document.getElementById('minutes').innerHTML = workTime--;
+}
+
+function addTime(){
+    document.getElementById('minutes').innerHTML = workTime++;
+
+}
+
+function start() {
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('reset').style.display = 'block';
+
+    
+    //change the timer
+    seconds = 5;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 1;
+
+    // countdown
+
+    let timerFunction = () => {
+
+        //change display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start
+        seconds = seconds - 1;
+
+        if(seconds === 0){
+            workMinutes--;
+            
+            if(workMinutes === -1){
+                // start break
+
+                if(breakCount % 2 == 0) {
+                workMinutes = breakMinutes;
+                breakCount++;
+            } else {
+                // continue work
+                workMinutes = workTime;
+                breakCount++;
+            }
+        }
+
+            seconds = 59;
+        }
+
+    }
+
+    // reset
+
+
+    var interval;
+
+
+    window.interval = setInterval(timerFunction, 1000) // 1000 = 1s;
+    
+}
